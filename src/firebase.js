14703490ab95a11969dd1d4fa34b2fa0f03f8d90
Abhishek Aggarwal <95749290+Abhishek0113@users.noreply.github.com>
2025-01-24 12:00:11 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDVFA9eg-_XmG_ynDYMCNo43pAc_4U6uEM",
-  authDomain: "placement-200eb.firebaseapp.com",
-  projectId: "placement-200eb",
-  storageBucket: "placement-200eb.appspot.com",
-  messagingSenderId: "846283037666",
-  appId: "1:846283037666:web:ad79dcd66d1ddc099777bb",
-  measurementId: "G-NN60VTK2J8",
+const firebaseConfig = { //add your credentials 
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
